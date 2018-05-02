b4c2e934b9824bdcf90e34eee4c00ddac8121ea6
root <root@master>
2018-05-02 10:53:48 +0000

    LAMP configuration via ansible using roles and handlers

---
+++ install_mysql.sh
@@ -0,0 +1,3 @@
+echo "mysql-server-5.6 mysql-server/root_password password root" | sudo debconf-set-selections
+echo "mysql-server-5.6 mysql-server/root_password_again password root" | sudo debconf-set-selections
+apt-get install -y mysql-server
